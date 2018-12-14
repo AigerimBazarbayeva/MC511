16ef45249dd6b5849fe4b6c4ce3da4183668cac0
Bazarbayeva <aigerim.bazarbayeva@dynatrace.com>
2018-12-14 05:37:58 +0100

    Prototype bar charting

--- app/src/main/java/com/at/fhooe/mhs/mobilehealthsportsapp/MainActivity.java
+++ app/src/main/java/com/at/fhooe/mhs/mobilehealthsportsapp/MainActivity.java
@@ -1,6 +1,5 @@
 package com.at.fhooe.mhs.mobilehealthsportsapp;
 
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
